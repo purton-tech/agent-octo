56773994b2bed354a78f1c7a5bda43eb6cbcd9f3
tiswas247 <tiswas247@users.noreply.github.com>
2026-03-04 13:51:44 +0000

    Try build

--- crates/octo-assets/build.rs
+++ crates/octo-assets/build.rs
@@ -4,7 +4,8 @@ use std::path::PathBuf;
 
 fn main() {
     let static_out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let asset_dirs = vec![PathBuf::from("./images"), PathBuf::from("./dist")];
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let asset_dirs = vec![manifest_dir.join("images"), manifest_dir.join("dist")];
 
     generate_static_files_code(&static_out_dir, &asset_dirs, &[]).unwrap();
 }
